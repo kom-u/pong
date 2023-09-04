80f1159521f48303249bf0061327be1a00546084
Atqa Munzir <atqamz@gmail.com>
2023-09-04 14:56:45 +0700

    adding push library

--- main.lua
+++ main.lua
@@ -1,24 +1,49 @@
+-- push https://github.com/Ulydev/push
+local push = require 'push'
+-- Sets virtual resolution
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Sets up the game window size
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+
+
 -- Called when the game first starts up
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text and graphics
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 end
 
+
+
+function love.keypressed(key)
+    -- Keys can be accessed by string name
+    if key == 'escape' then
+        -- function LOVE gives us to terminate application
+        love.event.quit()
+    end
+end
+
+
+
+-- Called after update by LOVE2D, used to draw anything to the screen, updated or otherwise
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center'                -- alignment mode, can be 'center', 'left', or 'right'
-    )
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    -- note we are now using virtual width and height now for text placement
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
 end
 
 
